bb7b7b791bb7b26059a5a28e444e0c13619de7df
Aric <haifeng.zhou@capgemini.com>
2020-12-02 23:19:56 +0800

    update code

--- app/src/main/java/com/capgemini/com/SmartApplication.kt
+++ app/src/main/java/com/capgemini/com/SmartApplication.kt
@@ -2,13 +2,14 @@ package com.capgemini.com
 
 import com.alibaba.android.arouter.launcher.ARouter
 import com.capgemini.lib_common.base.BaseApplication
+import com.capgemini.lib_common.extendtions.isTrue
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class SmartApplication: BaseApplication() {
     override fun onCreate() {
         super.onCreate()
-        ARouter.openDebug()
+        BuildConfig.DEBUG.isTrue {  ARouter.openDebug() }
         ARouter.init(this)
     }
 }
